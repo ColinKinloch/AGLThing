17ad798236fb83698d56e88a8bd50196364ea944
Colin Kinloch <colin@kinlo.ch>
2016-03-11 00:38:06 +0000

    glb and gltf with buffers.

--- src/GLThing.cpp
+++ src/GLThing.cpp
@@ -61,51 +61,81 @@ void GLThing::on_startup() {
   window->add(*area);*/
   window->show();
 
-  auto stream = Gio::File::create_for_uri("resource:///org/colinkinloch/glthing/model/duck.glb")->read();
-
-  char* magic = new char[4];
-  uint8_t* body;
-  stream->read(magic, 4);
-  if(string(magic, 4) != "glTF") throw "File not glTF";
-
-  uint32_t version;
-  stream->read(&version, 4);
-  switch(version) {
-    case 1:
-      break;
-    default:
-      cerr<<"Formats other than glTF v1.0 are not supported."<<endl;
-  }
-
-  uint32_t length;
-  uint32_t sceneLength;
-  stream->read(&length, 4);
-  stream->read(&sceneLength, 4);
-
-  uint32_t sceneFormat;
-  stream->read(&sceneFormat, 4);
-  switch(sceneFormat) {
-    case 0:
-      break;
-    default:
-      throw "Only JSON scenes are supported";
-  }
-  char* sceneData = new char [sceneLength];
-  stream->read(sceneData, sceneLength);
-
   glTFb gltf;
-  gltf.scene = json::parse(sceneData);
 
-  size_t bodyLength = length - stream->tell();
-  body = new uint8_t[bodyLength];
-  stream->read(body, bodyLength);
+  auto file = Gio::File::create_for_uri("resource:///org/colinkinloch/glthing/model/duck.glb");
+  auto stream = file->read();
 
-  gltf.buffers.insert({"binary_glTF", body});
+  std::string type = file->query_info()->get_content_type();
+  if(type == "application/octet-stream") {
+    cout<<"glb"<<endl;
+
+    {
+      char m;
+      for(char c: string("glTF")) {
+        stream->read(&m, 1);
+        if(c != m) throw "File not glTF";
+      }
+    }
+
+    {
+      uint32_t version;
+      stream->read(&version, 4);
+      switch(version) {
+        case 1:
+          break;
+        default:
+          cerr<<"Formats other than glTF v1.0 are not supported."<<endl;
+      }
+    }
+
+    uint32_t length;
+    uint32_t sceneLength;
+    stream->read(&length, 4);
+    stream->read(&sceneLength, 4);
+
+    uint32_t sceneFormat;
+    stream->read(&sceneFormat, 4);
+    switch(sceneFormat) {
+      case 0:
+        break;
+      default:
+        throw "Only JSON scenes are supported";
+    }
+
+    char* sceneData = new char [sceneLength];
+    stream->read(sceneData, sceneLength);
+
+    gltf.scene = json::parse(string(sceneData, sceneLength));
+
+    {
+      size_t bodyLength = length - stream->tell();
+      uint8_t* body = new uint8_t[bodyLength];
+      stream->read(body, bodyLength);
+      gltf.buffers.insert({"binary_glTF", body});
+    }
+  } else if (type == "text/plain") {
+    cout<<"json"<<endl;
+    char* scene;
+    size_t sceneLength;
+    file->load_contents(scene, sceneLength);
+    gltf.scene = json::parse(string(scene, sceneLength));
+  } else {
+    cout<<"Unknown type: "<<type<<endl;
+  }
+
+
+
   {
     json b = gltf.scene["buffers"];
     for(json::iterator it = b.begin(); it != b.end(); ++it) {
       if(it.key() == "binary_glTF") continue;
-      cout<<it.key()<<endl;
+      cout<<it.key()<<':'<<it.value()["uri"]<<endl;
+      auto f = file->get_parent()->resolve_relative_path(it.value()["uri"]);
+      uint8_t* body;
+      size_t length;
+      f->load_contents((char*&)body, length);
+      gltf.buffers.insert({it.key(), body});
     }
   }
   {
